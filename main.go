067a79e518feafa4bf90843a5a529ac956ac5a8d
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2024-07-18 10:07:57 -0300

    Golang - Variables & gitignore (#3)

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main // si el archivo no se encuentra en una carpeta, el package es main
 
-import "fmt" // importa el paquete fmt
+import ( 
+	//"fmt" // importa el paquete fmt
+	"github.com/Lelo88/dodesde0/variables"// importa el paquete github.com/desde0/desde0
+)
 
 func main() { // funcion principal
-	fmt.Println("Hola mundo")
+	// fmt.Println("Hola mundo")
+
+	variables.MostrarEnteros()
 }
 
