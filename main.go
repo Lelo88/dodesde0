a4b79981af9c655387b4f85af8f4135a116c3ebc
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2024-09-02 09:44:13 -0300

    Feature/curso golang 18 go routine (#19)
    * Curso Golang 18 - go routines
    
    * Curso Golang 18a - channel & goroutines

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main // si el archivo no se encuentra en una carpeta, el package es main
 import (
 	// "github.com/Lelo88/dodesde0/mapas"
 	// "github.com/Lelo88/dodesde0/users"
-	"github.com/Lelo88/dodesde0/deferpanic"
+	//"github.com/Lelo88/dodesde0/deferpanic"
+	//"fmt"
+
+	"github.com/Lelo88/dodesde0/goroutine"
 	//interfaces "github.com/Lelo88/dodesde0/ejer_interfaces"
 	//"github.com/Lelo88/dodesde0/modelos"
 )
@@ -76,5 +79,19 @@ func main() { // funcion principal
 	//deferpanic.VemosDefer()
 
 	//deferpanic.EjemploPanic()
-	deferpanic.EjemploRecover()
+	//deferpanic.EjemploRecover()
+
+	canal1 := make(chan bool)
+
+	go goroutine.MiNombreLentooo("Leandro", canal1)
+	// llamo a la rutina y se ejecuta por ser asíncrono, por lo que no sabemos si se ejecutó o no
+	// Vamos a crear un pequeño código de ejemplo
+	defer func() {
+		// no va a terminar la ejecución hasta que todos los 
+		<-canal1 // el canal es el que está enviando información. Esto sería un await
+	}()
+	
+	// var x string
+	// fmt.Scanln(&x) // el programa se detiene hasta que el usuario presione enter u otra tecla
+
 }
